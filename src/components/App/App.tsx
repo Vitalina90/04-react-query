c5aeb488b45a780027fd9e7f47e34747ec639344
Vitalina <vsinkovaa@gmail.com>
2025-06-02 23:48:53 +0300

    hw-04-react-query

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,67 +1,86 @@
 import { useState } from "react";
-import { fetchMovies } from "../../services/movieService";
+import { useQuery } from "@tanstack/react-query";
 import toast, { Toaster } from 'react-hot-toast';
+import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
+
 import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 
-export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
+import css from "../App/App.module.css"
 
-  const handleSearch = async (query: string): Promise<void> => {
-    setMovies([]);
-    setLoading(true);
-    setError(false);
-    
-    try {
-      const results = await fetchMovies(query);
-      
-      if (results.length === 0) {
-        toast('No movies found for your request.');
-        return;
-      }
-      setMovies(results);
-    } catch {
-        setError(true);
-    } finally {
-      setLoading(false);
-    }
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const {
+    data,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['movie', query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: !!query,
+  });
+
+  useEffect(() => {
+  if (data && data.results.length === 0) {
+    toast('No movies found for your request.');
+  }
+ }, [data]);
+
+  const handleSubmit = (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
   };
 
   return (
     <>
       <Toaster />
 
-      <SearchBar
-        onSubmit={handleSearch} />
+      <SearchBar onSubmit={handleSubmit} />
 
-      {movies.length > 0 &&
-        <MovieGrid
-          movies={movies}
-          onSelect={setSelectedMovie} />
-      }
+      {isLoading && <Loader />}
+      
+      {isError && <ErrorMessage />}
 
-      {loading &&
-        <Loader />
-      }
-      
-      {error &&
-        <ErrorMessage />
-      }
-      
-      {selectedMovie &&
+      {data && data.results.length > 0 && (
+        <>
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+            <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
+
+        </>
+      )}
+
+      {selectedMovie && (
         <MovieModal
           movie={selectedMovie}
-          onClose={() => setSelectedMovie(null)} />
-      }
+          onClose={() => setSelectedMovie(null)}
+        />
+        
+      )}
     </>
   );
-    
 }
+
+
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-// Нормалізація стилів
 import "modern-normalize";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
